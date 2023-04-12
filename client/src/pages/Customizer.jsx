20d796bf9dc3cbcf212184b8f257d630249d8dec
FATEH BEDDIAF <62912697+madjed-hue@users.noreply.github.com>
2023-04-12 23:33:16 +0100

    final version

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useSnapshot } from "valtio";
 
 import state from "../store";
-
+import { reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
-import { ColorPicker, CustomButton, FilePicker, Tab } from "../components";
+import {
+  AIPicker,
+  ColorPicker,
+  CustomButton,
+  FilePicker,
+  Tab,
+} from "../components";
 
 const Customizer = () => {
   const snap = useSnapshot(state);
